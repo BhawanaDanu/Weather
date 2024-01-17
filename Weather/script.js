523895d242addc38befcd2cb7ecfc45beec9e29f
Bhawana <94728829+BhawanaDanu@users.noreply.github.com>
2024-01-17 22:48:40 +0530

    Add files via upload

---
+++ Weather/script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b99247ec5cmshedca2f590857d1ep1830efjsn087131102dec',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
